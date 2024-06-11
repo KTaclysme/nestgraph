0efc9f9388cec78b1aa8f98c2e9d3ea829d72863
KTaclysme <takevin.pro@gmail.com>
2024-06-11 23:20:30 +0200

    feat(src/user/user.resolvers):mutation and query deco

--- src/user/user.resolvers.ts
+++ src/user/user.resolvers.ts
@@ -1,7 +1,7 @@
-import { Param } from '@nestjs/common';
 import { UserService } from './user.service';
-import { Resolver, Query } from '@nestjs/graphql';
+import { Resolver, Query, Args, Mutation } from '@nestjs/graphql';
 import { User } from './models/user.model';
+import { UserToCreateDto } from './UserDto/UserToCreate.dto';
 
 @Resolver((of) => User)
 export class UserResolver {
@@ -11,12 +11,22 @@ export class UserResolver {
     // addUser(@Body() userToCreateDto: UserToCreateDto) {
     //     return this.userService.addUser(userToCreateDto.email);
     // }
+    @Mutation(returns => User)
+    async addUser(@Args('email') email :string): Promise<User> {
+        //return this.userService.addUser(email);
+    }
+    
+    @Query(returns => User)
+  async user(@Args('id') id: string): Promise<User> {
+    // return this.userService.findById(id);
+  }
 
-    @Query((returns) => User)
-    getUser(@Param('email') email: string) {
+    @Query(returns => User)
+    getUser(@Args('email') email: string): Promise<User> {
         // return this.userService.getUser(email);
     }
 
+    
     // @Delete()
     // resetData() {
     //     return this.userService.resetData();
