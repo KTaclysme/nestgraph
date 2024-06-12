9b0d5679bbaf04da335649c2458f4f2fcf1bdf0a
kevin.ta <kevin.ta@emeria.eu>
2024-06-12 11:38:58 +0200

    refacto(src/user): make graphql working

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,23 +1,23 @@
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
-import { UserService } from './user.service';
-import { UserToCreateDto } from './UserDto/UserToCreate.dto';
+// import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+// import { UserService } from './user.service';
+// import { UserToCreateDto } from './UserDto/UserToCreate.dto';
 
-@Controller('user')
-export class UserController {
-    constructor(private readonly userService: UserService) {}
+// @Controller('user')
+// export class UserController {
+//     constructor(private readonly userService: UserService) {}
 
-    @Post()
-    addUser(@Body() userToCreateDto: UserToCreateDto) {
-        // return this.userService.addUser(userToCreateDto.email);
-    }
+//     @Post()
+//     addUser(@Body() userToCreateDto: UserToCreateDto) {
+//          return this.userService.addUser(userToCreateDto.email);
+//     }
 
-    @Get(':email')
-    getUser(@Param('email') email: string) {
-        // return this.userService.getUser(email);
-    }
+//     @Get(':email')
+//     getUser(@Param('email') email: string) {
+//          return this.userService.getUser(email);
+//     }
 
-    @Delete()
-    resetData() {
-        // return this.userService.resetData();
-    }
-}
+//     @Delete()
+//     resetData() {
+//          return this.userService.resetData();
+//     }
+// }
--- src/user/user.resolvers.ts
+++ src/user/user.resolvers.ts
@@ -1,32 +1,29 @@
 import { UserService } from './user.service';
 import { Resolver, Query, Args, Mutation } from '@nestjs/graphql';
 import { User } from './models/user.model';
-import { UserToCreateDto } from './UserDto/UserToCreate.dto';
+// import { UserToCreateDto } from './UserDto/UserToCreate.dto';
 
 @Resolver((of) => User)
 export class UserResolver {
     constructor(private readonly userService: UserService) {}
 
-    // @Post()
-    // addUser(@Body() userToCreateDto: UserToCreateDto) {
-    //     return this.userService.addUser(userToCreateDto.email);
-    // }
-    @Mutation(returns => User)
-    async addUser(@Args('email') email :string): Promise<User> {
-        //return this.userService.addUser(email);
+    @Mutation((returns) => User)
+    async addUser(@Args('email') email: string): Promise<User> {
+        console.log('popo1');
+        return this.userService.addUser(email);
     }
-    
-    @Query(returns => User)
-  async user(@Args('id') id: string): Promise<User> {
-    // return this.userService.findById(id);
-  }
 
-    @Query(returns => User)
+    @Query((returns) => User)
+    async user(@Args('id') id: number): Promise<User> {
+        return this.userService.getUserId(id);
+    }
+
+    @Query((returns) => User)
     getUser(@Args('email') email: string): Promise<User> {
-        // return this.userService.getUser(email);
+        return this.userService.getUser(email);
     }
 
-    
+    //ajouter un delete
     // @Delete()
     // resetData() {
     //     return this.userService.resetData();
