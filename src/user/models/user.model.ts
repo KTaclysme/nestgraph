03fdf5879f321607747a5fb63d773ca68b79679f
KTaclysme <takevin.pro@gmail.com>
2024-06-13 23:08:18 +0200

    refacto(src/user): only sequelize

--- src/user/models/user.model.ts
+++ src/user/models/user.model.ts
@@ -1,15 +1,19 @@
 import { Field, ObjectType } from '@nestjs/graphql';
-import { Model, Table } from 'sequelize-typescript';
-import { Column, PrimaryGeneratedColumn } from 'typeorm';
+import { Model, Table, Column, AutoIncrement, PrimaryKey } from 'sequelize-typescript';
 
 @Table
 @ObjectType({ description: 'user' })
 export class User extends Model {
     @Field()
-    @PrimaryGeneratedColumn()
+    @PrimaryKey
+    @AutoIncrement
+    @Column
     id: number;
 
     @Field()
-    @Column('text', { nullable: false })
+    @Column({
+        type: 'text',
+        allowNull: false
+    })
     email: string;
 }
